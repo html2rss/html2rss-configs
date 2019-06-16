90ac7edce7d2aa9307ab2b72d9dd08cf64de482f
Gil Desmarais <gil@desmarais.de>
2019-06-16 09:07:01 +0200

    extract shared_example for use with each config yml

--- spec/html2rss/config_ymls_spec.rb
+++ spec/html2rss/config_ymls_spec.rb
@@ -1,44 +1,5 @@
 Html2rss::Configs.file_names.each do |file_name|
   RSpec.describe file_name do
-    subject(:yaml) { YAML.safe_load(file) }
-
-    let(:file) { File.open(file_name) }
-
-    it 'is parseable' do
-      expect { yaml }.not_to raise_error
-    end
-
-    it 'resides in topfolder named after channel.url\'s host' do
-      dirname = File.dirname(file.path).split(File::Separator).last
-      host_name = URI(yaml['channel']['url']).host.gsub('www.', '')
-
-      expect(dirname).to eq(host_name)
-    end
-
-    it 'has channel' do
-      expect(yaml).to have_key 'channel'
-    end
-
-    context 'with channel present' do
-      %w[title url ttl].each do |required_attribute|
-        it "has channel.#{required_attribute}" do
-          expect(yaml['channel']).to have_key required_attribute
-        end
-      end
-    end
-
-    it 'has selectors' do
-      expect(yaml).to have_key 'selectors'
-    end
-
-    context 'with selectors present' do
-      it { expect(yaml['selectors']).to have_key 'items' }
-
-      %w[items title].each do |required_attribute|
-        it "has selectors.#{required_attribute}" do
-          expect(yaml['selectors'][required_attribute]).not_to be_empty
-        end
-      end
-    end
+    include_examples 'config.yml', file_name
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'bundler/setup'
 require 'html2rss/configs'
 
+Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
