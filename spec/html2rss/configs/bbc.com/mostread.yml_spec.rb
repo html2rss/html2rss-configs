158f517da77b8976ef07a8019da1024d3c6b5829
Gil Desmarais <gildesmarais@users.noreply.github.com>
2021-03-28 00:49:46 +0100

    fix: repair or remove broken configs (#119)

--- spec/html2rss/configs/bbc.com/mostread.yml_spec.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe 'bbc.com/mostread.yml' do
-  include_examples 'config.yml', 'bbc.com/mostread.yml'
-end
--- spec/html2rss/configs/nwzonline.de/meistgelesen.yml_spec.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe 'nwzonline.de/meistgelesen.yml' do
-  include_examples 'config.yml', 'nwzonline.de/meistgelesen.yml'
-end
