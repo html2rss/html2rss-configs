9267e6cffb04866ae6fc15756e87777c76fe1654
Gil Desmarais <git@desmarais.de>
2021-05-28 22:59:49 +0200

    feat: symbolize names when returning config

--- lib/html2rss/configs.rb
+++ lib/html2rss/configs.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'active_support/core_ext/hash'
 require 'html2rss/configs/version'
 require 'yaml'
 
@@ -11,10 +12,15 @@ module Html2rss
 
     class ConfigNotFound < Html2rss::Configs::Error; end
 
+    ##
+    # @return [Array<String>]
     def self.file_names
       @file_names ||= Dir[File.join(__dir__, '**', '*.yml')].freeze
     end
 
+    ##
+    # @param name [String] the name of the config to find. format: `domainname/name`
+    # @return [Hash<Symbol, Object>] the hash to create a Html2rss::Config
     def self.find_by_name(name)
       raise 'name must be a string' unless name.is_a?(String)
       raise 'name must be in folder/file format' unless name.include?('/')
@@ -25,7 +31,7 @@ module Html2rss
 
       raise ConfigNotFound unless file_name
 
-      YAML.safe_load(File.open(file_name)).freeze
+      YAML.safe_load(File.open(file_name), symbolize_names: true).freeze
     end
   end
 end
