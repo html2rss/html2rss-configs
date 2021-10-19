5d9b23ddc57a9e3369970f79d4f85122eba3a368
Gil Desmarais <git@desmarais.de>
2021-10-19 11:58:11 +0200

    feat: add ingenieur mell

---
+++ spec/html2rss/configs/ingenieur.de/karriere_arbeitsleben_heiko_mell.yml_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe 'ingenieur.de/karriere_arbeitsleben_heiko_mell.yml' do
+  include_examples 'config.yml', 'ingenieur.de/karriere_arbeitsleben_heiko_mell.yml'
+end
