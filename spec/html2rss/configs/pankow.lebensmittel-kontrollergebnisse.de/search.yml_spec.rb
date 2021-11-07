c4a87e25252438a7cb9e0d22f1717580c0cfe347
Gil Desmarais <gildesmarais@users.noreply.github.com>
2021-11-07 17:43:57 +0100

    feat: add pankow lebensmittel-kontrollergebnisse (#151)

---
+++ spec/html2rss/configs/pankow.lebensmittel-kontrollergebnisse.de/search.yml_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe 'pankow.lebensmittel-kontrollergebnisse.de/search.yml' do
+  include_examples 'config.yml', 'pankow.lebensmittel-kontrollergebnisse.de/search.yml'
+end
