233327be18967c7a36b8a5648aa7e428e1daa72a
Gil Desmarais <git@desmarais.de>
2026-08-16 16:41:38 +0200

    feat(configs): add P0 IGO and finance primary feeds
    Ship eight civic/finance/science configs (WHO, World Bank, OECD, Europarl,
    Consilium, SIPRI, Bundesregierung, Carnegie). Drop State.gov (fetch-lane
    Botasaurus timeout), IMF/OECD CF blocks without Faraday, Commission RSS,
    BRICS 401, and BIS first-party RSS.

--- spec/support/botasaurus_fetch_configs.rb
+++ spec/support/botasaurus_fetch_configs.rb
@@ -3,10 +3,17 @@
 module BotasaurusFetchConfigs
   CONFIGS = %w[
     apple.com/newsroom.yml
+    bundesregierung.de/pressemitteilungen.yml
+    carnegieendowment.org/research.yml
     imdb.com/ratings.yml
+    oecd.org/newsroom.yml
     stackoverflow.com/hot_network_questions.yml
     support.apple.com/en_gb_ht201222.yml
     thoughtworks.com/insights.yml
+    who.int/news.yml
+    worldbank.org/news.yml
+    www.consilium.europa.eu/press-releases.yml
+    www.europarl.europa.eu/press-room.yml
   ].freeze
 
   module_function
